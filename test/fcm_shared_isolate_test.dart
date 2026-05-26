e91c00dcd65f024b7b4981436856d41e44e84028
yurtemre7 <e.yurtseven@famedly.com>
2026-05-26 16:04:45 +0200

    chore: format and ignore fvm and vscode config files

--- test/fcm_shared_isolate_test.dart
+++ test/fcm_shared_isolate_test.dart
@@ -10,8 +10,8 @@ void main() {
   setUp(() {
     TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
         .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
-      return '42';
-    });
+          return '42';
+        });
   });
 
   tearDown(() {
